9084f4d4db15947114cb84b11153b0d48b037aa4
Anny <70793585+anyast103@users.noreply.github.com>
2022-07-08 23:54:34 +0300

    Добавлена таблица для генерации списков друзей.

--- models/user.py
+++ models/user.py
@@ -16,6 +16,13 @@ class UserAchievements(BaseModel):
     achievement_id = Column(Integer, ForeignKey('achievement.id', ondelete='CASCADE'), nullable=False, index=True)
 
 
+class UserFriend(BaseModel):
+    __tablename__ = 'user_friends'
+
+    user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False, index=True)
+    friend_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False, index=True)
+
+
 class UserInterests(BaseModel):
     __tablename__ = 'user_interests'
 
