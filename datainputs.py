17e4bec3ddcac98e41ade3ebd0738eb22d309c45
Vlad <luckyvalenok1@gmail.com>
2022-07-09 15:42:20 +0500

    Убран лишний DataInput

--- datainputs.py
+++ datainputs.py
@@ -215,8 +215,6 @@ data_inputs = [
                              lambda n: Interest(name=n)),
     ManageSomethingDataInput(Step.INTEREST_NAME_FOR_REMOVE, Step.NONE, 'Интерес', Interest,
                              Interest.name, None),
-    ManageSomethingDataInput(Step.ACHIEVEMENT_NAME_FOR_ADD, Step.NONE, 'Достижение', Achievement,
-                             Achievement.name, lambda n: Achievement(name=n)),
     ManageSomethingDataInput(Step.ACHIEVEMENT_NAME_FOR_REMOVE, Step.NONE, 'Достижение', Achievement,
                              Achievement.name, None),
     ManageSomethingDataInput(Step.GROUP_NAME_FOR_ADD, Step.NONE, 'Группа', LocalGroup, LocalGroup.name,
